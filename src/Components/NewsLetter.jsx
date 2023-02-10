aef7d7fc788f1b0908459a9c02f69aaa0a2f2b4f
jahanzaib2630 <jahanzaib@clustox.com>
2023-02-10 16:36:40 +0500

    newsletter component completed

--- src/Components/NewsLetter.jsx
+++ src/Components/NewsLetter.jsx
@@ -1,64 +1,73 @@
 import React from "react";
 import { Button, Input } from "antd";
+import styled from "styled-components";
 const NewsLetter = () => {
-  const stylesNewsLetter = {
-    alignCenter: {
-      textAlign: "center",
-      marginTop: "55px",
-      marginBottom: "60px",
-    },
-    text1: { fontWeight: "500", fontSize: "22px" },
-    text2: { fontWeight: "700", fontSize: "35px" },
-    text3: { fontWeight: "400", fontSize: "16px" },
-    btnColor: {
-      backgroundColor: "black",
-      color: "white",
-      fontWeight: "600",
-      height:"40px"
-    },
-    text1Div: {
-      paddingBottom: "30px",
-    },
-    text2Div: {
-      paddingBottom: "30px",
-    },
-    text3DivSubParent: { width: "700px", paddingBottom: "40px" },
-  };
+  const TextOne = styled.span`
+    font-weight: 500;
+    font-size: 22px;
+  `;
+  const TextTwo = styled.span`
+    font-weight: 700;
+    font-size: 35px;
+    @media only screen and (max-width: 768px) {
+      font-size: 19px;
+    }
+  `;
+  const TextThree = styled.span`
+    font-weight: 400;
+    font-size: 16px;
+  `;
+  const BtnColor = styled(Button)`
+    background-color: black;
+    color: white;
+    font-weight: 600;
+    height: 40px;
+  `;
+  const MainDiv = styled.div`
+    text-align: center;
+    margin-top: 55px;
+    margin-bottom: 60px;
+  `;
+  const DivPaddingBottom = styled.div`
+    paddind-bottom: 30px;
+  `;
+  const Text3DivSubParent = styled.div`
+    width: 55%;
+    padding-bottom: 40px;
+  `;
   return (
-    <div style={stylesNewsLetter.alignCenter}>
+    <MainDiv>
       <div>
-        <div style={stylesNewsLetter.text1Div}>
-          <span style={stylesNewsLetter.text1}>NEWSLETTER</span>
-        </div>
-        <div style={stylesNewsLetter.text2Div}>
-          <span style={stylesNewsLetter.text2}>
-            Subscribe Our To Newsletter
-          </span>
-        </div>
+        <DivPaddingBottom>
+          <TextOne>NEWSLETTER</TextOne>
+        </DivPaddingBottom>
+        <DivPaddingBottom>
+          <TextTwo>Subscribe Our To Newsletter</TextTwo>
+        </DivPaddingBottom>
         <div className="d-flex justify-content-center">
-          <div style={stylesNewsLetter.text3DivSubParent}>
-            <span style={stylesNewsLetter.text3}>
+          <Text3DivSubParent>
+            <TextThree>
               KLorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
               KLorem ipsum dolor sit amet, consectetur adipiscing elit. KLorem
               ipsum dolor sit amet, consectetur
-            </span>
-          </div>
+            </TextThree>
+          </Text3DivSubParent>
         </div>
       </div>
       <div>
         <Input.Group compact>
           <Input
             style={{
-              width: "calc(50% - 200px)",
-              height:"40px"
+              width: "50%",
+              height: "40px",
             }}
             placeholder={"Your Email Address"}
             //   defaultValue="https://ant.design"
           />
-          <Button style={stylesNewsLetter.btnColor}>Subscribe</Button>
+          <BtnColor>Subscribe</BtnColor>
         </Input.Group>
       </div>
-    </div>
+    </MainDiv>
   );
 };
 
